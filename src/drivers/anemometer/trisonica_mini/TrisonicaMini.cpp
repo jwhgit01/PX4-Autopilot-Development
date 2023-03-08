ebdc4a10498f1311e305180974a830dea9ed0ec7
jwhgit01 <jeremyhopwood@vt.edu>
2023-03-08 17:32:01 -0500

    Commit with initial driver setup, before moving to cubeorange plus.

---
+++ src/drivers/anemometer/trisonica_mini/TrisonicaMini.cpp
@@ -0,0 +1,231 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2014-2019, 2021 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "TrisonicaMini.hpp"
+#include <lib/drivers/device/Device.hpp>
+
+
+TrisonicaMini::TrisonicaMini(const char *port) :
+	ScheduledWorkItem(MODULE_NAME, px4::serial_port_to_wq(port))
+{
+	/* store port name */
+	strncpy(_port, port, sizeof(_port) - 1);
+
+	/* enforce null termination */
+	_port[sizeof(_port) - 1] = '\0';
+
+	// device::Device::DeviceId device_id;
+	// device_id.devid_s.bus_type = device::Device::DeviceBusType_SERIAL;
+
+	// uint8_t bus_num = atoi(&_port[strlen(_port) - 1]); // Assuming '/dev/ttySx'
+
+	// if (bus_num < 10) {
+	// 	device_id.devid_s.bus = bus_num;
+	// }
+
+
+
+}
+
+TrisonicaMini::~TrisonicaMini()
+{
+	stop();
+
+	perf_free(_sample_perf);
+	perf_free(_comms_errors);
+}
+
+int TrisonicaMini::init()
+{
+	start();
+
+	return PX4_OK;
+}
+
+int TrisonicaMini::collect()
+{
+	perf_begin(_sample_perf);
+
+	/* create variables to be populated by the parser */
+	// float V_m_s, direction_deg, u_m_s, v_m_s, w_m_s, T_C;
+	// bool valid = false;
+
+	/* clear buffer if last read was too long ago */
+	int64_t read_elapsed = hrt_elapsed_time(&_last_read);
+
+	/* read from the sensor (uart buffer) */
+	// const hrt_abstime timestamp_sample = hrt_absolute_time();
+	char readbuf[sizeof(_buffer)];
+	int bytes_read = ::read(_file_descriptor, &readbuf[0], sizeof(readbuf));
+
+	/* handle read errors */
+	if (bytes_read < 0) {
+		PX4_DEBUG("read err: %d", bytes_read);
+		perf_count(_comms_errors);
+		perf_end(_sample_perf);
+		if (read_elapsed > (_interval * 2)) {
+			return bytes_read;
+		} else {
+			return -EAGAIN;
+		}
+	} else if (bytes_read == 0) {
+		return -EAGAIN;
+	}
+
+	_last_read = hrt_absolute_time();
+	PX4_INFO("Collection starting.\n");
+
+	/* loop through read buffer and parse data */
+	// for (int i = 0; i < bytes_read; i++) {
+	// 	if (1 == trisonica_mini_parser(readbuf[i],_buffer,&_buffer_index,&_parse_state,&V_m_s,&direction_deg,&u_m_s,&v_m_s,&w_m_s,&T_C)) {
+	// 		valid = true;
+	// 	}
+	// }
+
+	// if (!valid) {
+	// 	return -EAGAIN;
+	// }
+
+	// /* create and polulate the topic to be published */
+	// sensor_anemometer_s report{};
+	// report.timestamp = timestamp_sample;
+	// report.V_m_s = V_m_s;
+	// report.direction_deg = direction_deg;
+	// report.u_m_S = u_m_s;
+	// report.v_m_s = v_m_s;
+	// report.w_m_s = w_m_s;
+	// report.T_C = T_C;
+
+	// /* publish the sensor readings */
+	// _sensor_anemometer_pub.publish(report);
+
+	perf_end(_sample_perf);
+
+	return PX4_OK;
+}
+
+int TrisonicaMini::open_serial_port(const speed_t speed)
+{
+	/* file descriptor initialized? */
+	if (_file_descriptor > 0) {
+		PX4_DEBUG("serial port already open");
+		return PX4_OK;
+	}
+
+	/* configure port flags for read/write, non-controlling, non-blocking. */
+	int flags = (O_RDWR | O_NOCTTY | O_NONBLOCK);
+
+	/* open the serial port. */
+	_file_descriptor = ::open(_port, flags);
+
+	/* check for errors */
+	if (_file_descriptor < 0) {
+		PX4_ERR("open failed (%i)", errno);
+		return PX4_ERROR;
+	}
+	if (!isatty(_file_descriptor)) {
+		PX4_WARN("not a serial device");
+		return PX4_ERROR;
+	}
+
+	struct termios uart_config;
+	int termios_state;
+
+	/* fill the struct for the new configuration */
+	tcgetattr(_file_descriptor, &uart_config);
+	uart_config.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR | PARMRK | INPCK | ISTRIP | IXON);
+
+	/* Clear ONLCR flag (which appends a CR for every LF). */
+	uart_config.c_oflag &= ~ONLCR;
+
+	/* No parity, one stop bit. */
+	uart_config.c_cflag &= ~(CSTOPB | PARENB);
+
+	/* No line processing - echo off, echo newline off, canonical mode off, extended input processing off, signal chars off */
+	uart_config.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
+
+	/* set baud rate and check for errors*/
+	if ((termios_state = cfsetispeed(&uart_config, speed)) < 0) {
+		PX4_ERR("CFG: %d ISPD", termios_state);
+		::close(_file_descriptor);
+		return PX4_ERROR;
+	}
+	if ((termios_state = cfsetospeed(&uart_config, speed)) < 0) {
+		PX4_ERR("CFG: %d OSPD", termios_state);
+		::close(_file_descriptor);
+		return PX4_ERROR;
+	}
+	if ((termios_state = tcsetattr(_file_descriptor, TCSANOW, &uart_config)) < 0) {
+		PX4_ERR("baud %d ATTR", termios_state);
+		::close(_file_descriptor);
+		return PX4_ERROR;
+	}
+
+	PX4_INFO("successfully opened UART port %s", _port);
+	return PX4_OK;
+}
+
+void TrisonicaMini::Run()
+{
+	/* make sure the serial port is open */
+	open_serial_port();
+
+	/* perform collection */
+	int ret_col = collect();
+	if(ret_col < 0){
+		PX4_INFO("RETCOL was less than 0\n");
+	}
+
+	/* Possible TODO: do something based on ret_col */
+}
+
+void TrisonicaMini::start()
+{
+	/* schedule the driver at regular intervals */
+	ScheduleOnInterval(READ_INTERVAL);
+}
+
+void TrisonicaMini::stop()
+{
+	/* Ensure the serial port is closed. */
+	::close(_file_descriptor);
+
+	/* Clear the work queue schedule. */
+	ScheduleClear();
+}
+
+void TrisonicaMini::print_info()
+{
+	perf_print_counter(_sample_perf);
+	perf_print_counter(_comms_errors);
+}
---
+++ src/drivers/anemometer/trisonica_mini/parser.cpp
@@ -0,0 +1,118 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2014 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file parser.cpp
+ * @author Jeremy W. Hopwood
+ *
+ * Parser for the TriSonica Mini 3D sonic anemometer
+ */
+
+#include "parser.hpp"
+
+/*
+ * This function parses data from a read buffer one character
+ * at a time and returns a valid packet once assembled.
+ *
+ * This parser relies on the trisonica mini anemometer data
+ * packets to be configured as follows:
+ *
+ * 	S 05.2 D 112 U -01.9 V 04.7 W 01.1 T 22.6 \r\n
+ *
+ * The tags are in the following format and order:
+ *	S = Wind Speed
+ *	D = Wind Direction
+ *	U = U-Vector
+ *	V = V-Vector
+ *	W = W-Vector
+ *	T = Temperature
+ *
+ */
+int trisonica_mini_parser(char c,char *buffer,int *buffer_index,int *parse_state,float *S,float *D,float *U,float *V,float *W,float *T){
+
+	/* If we are lost, look for a start or end character */
+	if ( (*parse_state)<1 ){
+		switch (c) {
+		case START_PACKET:
+			buffer[0] = c;
+			(*buffer_index) = 1;
+			(*parse_state) = 1;
+			break;
+
+		case END_PACKET:
+			(*buffer_index)++;
+			(*parse_state) = 1;
+			break;
+
+		default:
+			(*buffer_index)++;
+			break;
+		}
+		return 0;
+	}
+
+	/* If we have gotten to here, we are not lost and are either
+	   indexed at the start, middle, or end of a packet. */
+	switch (c) {
+
+		/* If we get a start-of-packet, begin storing it. */
+		case START_PACKET:{
+			buffer[0] = c;
+			(*buffer_index) = 1;
+			return 0;
+			break;
+		}
+
+		/* If we get an end-of-packet character, return the valid buffer. */
+		case END_PACKET:{
+			buffer[*buffer_index] = '\0';
+			int scan_result = sscanf(buffer,"S%f D%f U%f V%f W%f T%f\r",S,D,U,V,W,T);
+			if (scan_result != 6){
+				(*parse_state) = 0;
+				return 0;
+			}
+			/* If the result is good, reset the index to zero and return OK */
+			(*buffer_index) = 0;
+			return 1;
+			break;}
+
+		/* If it's not a start or end of packet, put it in the buffer to be scanned. */
+		default:{
+			buffer[*buffer_index] = c;
+			(*buffer_index)++;
+			return 0;
+			break;
+		}
+	}
+
+}
